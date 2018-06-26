02b97161e023caf3d6d04fff7b12e39f793ee765
Pedro Okawa <pedrookawa@Pedros-MacBook-Pro.local>
2018-06-27 00:21:24 +0100

    First commit

---
+++ app/src/main/java/com/okawa/minichat/ui/main/MainFragment.kt
@@ -0,0 +1,24 @@
+package com.okawa.minichat.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.okawa.minichat.R
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+
+}
